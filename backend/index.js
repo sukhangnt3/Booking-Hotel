d82289c7a35ae5a1c842b692ca75a7da4f9bd90c
sukhangnt3 <sukhangnt3@gmail.com>
2026-09-02 01:31:40 +0700

    big update

--- backend/index.js
+++ backend/index.js
@@ -36,7 +36,7 @@ app.use(
   }),
 );
 
-app.use(express.json({ limit: "1mb" }));
+app.use(express.json({ limit: "50MB" }));
 
 // ─── SWAGGER UI: http://localhost:5000/api/docs ───
 app.use("/api/docs", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
