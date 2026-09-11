f201b886e7d5481bfa3d1b383d9fea809174ee23
sukhangnt3 <sukhangnt3@gmail.com>
2026-09-11 19:11:48 +0700

    update database.js

--- backend/config/database.js
+++ backend/config/database.js
@@ -6,6 +6,10 @@ const pool = new Pool({
   user: process.env.DB_USER || "postgres",
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME || "hotel_booking",
+
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
 
 module.exports = pool;
