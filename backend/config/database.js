9641fae7fc1100f6613e808f8ea2e00db72a1d4b
sukhangnt3 <sukhangnt3@gmail.com>
2026-09-11 21:38:38 +0700

    update web

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,15 +1,19 @@
 const { Pool } = require("pg");
 
+const isProduction = process.env.NODE_ENV === "production";
+
 const pool = new Pool({
   host: process.env.DB_HOST || "localhost",
   port: Number(process.env.DB_PORT || 5432),
   user: process.env.DB_USER || "postgres",
-  password: process.env.DB_PASSWORD,
+  password: process.env.DB_PASSWORD || "",
   database: process.env.DB_NAME || "hotel_booking",
 
-  ssl: {
-    rejectUnauthorized: false,
-  },
+  ...(isProduction && {
+    ssl: {
+      rejectUnauthorized: false,
+    },
+  }),
 });
 
 module.exports = pool;
