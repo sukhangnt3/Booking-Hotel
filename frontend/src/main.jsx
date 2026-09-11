215de952d8b6a94b17fb10460b440ec07d4af110
sukhangnt3 <sukhangnt3@gmail.com>
2026-09-11 19:55:05 +0700

    fix lag

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -2,7 +2,6 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
-import "flag-icons/css/flag-icons.min.css";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import path from "path";
-import { fileURLToPath } from "url"; // Thêm dòng này
+import { fileURLToPath } from "url";
 
 // Tự định nghĩa __dirname cho môi trường ES Modules
 const __filename = fileURLToPath(import.meta.url);
@@ -15,5 +15,11 @@ export default defineConfig({
       // Dấu @ bây giờ sẽ trỏ thẳng vào thư mục src
       "@": path.resolve(__dirname, "./src"),
     },
+  },
+  build: {
+    // Ngăn Vite nén hàng loạt file SVG thành chuỗi base64 trong code, giúp giảm tải bước Optimizer
+    assetsInlineLimit: 0,
+    // Tăng giới hạn cảnh báo dung lượng file để tránh tiến trình bị dừng hoặc chậm
+    chunkSizeWarningLimit: 3000,
   },
 });
